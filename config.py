4331b43ae9775911c7b82a05be6085d2ac3dd147
Vinay <vinayvobbilichetty11@gmail.com>
2024-11-20 13:50:38 -0500

    more changes

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ def load_config():
     return Config(
         bot_api_token=os.environ["BOT_API_TOKEN"],
         webex_recipient_room_id=os.environ["WEBEX_RECIPIENT_ROOM_ID"],
-        xsoar_api_url=os.environ["XSOAR_API_URL"],
+        xsoar_api_base_url=os.environ["XSOAR_API_URL"],
         xsoar_auth_token=os.environ["XSOAR_AUTH_TOKEN"],
         xsoar_auth_id=os.environ["XSOAR_AUTH_ID"]
     )
@@ -21,7 +21,7 @@ class Config:
     """Configuration settings for the application."""
     bot_api_token: str
     webex_recipient_room_id: str
-    xsoar_api_url: str
+    xsoar_api_base_url: str
     xsoar_auth_token: str
     xsoar_auth_id: str
     webex_api_url: str = "https://webexapis.com/v1/messages"
