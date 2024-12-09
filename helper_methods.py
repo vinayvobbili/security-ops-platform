b2a1cb18274da34dbca5c49582abdd7b5fb2f6fe
Vinay <vinayvobbilichetty11@gmail.com>
2024-12-09 15:33:48 -0500

    more changes

--- helper_methods.py
+++ helper_methods.py
@@ -12,8 +12,11 @@ def log_activity(func):
         activity = args[3]
 
         now_eastern = datetime.now(eastern).strftime('%m/%d/%Y %I:%M:%S %p %Z')
-        with open("activity_log.csv", "a") as f:
-            f.write(f"{activity['actor']['displayName']},{attachment_actions.json_data['text']},{get_room_name(attachment_actions.json_data['roomId'])},{now_eastern}\n")
+        try:
+            with open("activity_log.csv", "a") as f:
+                f.write(f"{activity['actor']['displayName']},{attachment_actions.json_data.get('inputs', {}).get('command_keyword') or attachment_actions.json_data['text']},{get_room_name(attachment_actions.json_data['roomId'])},{now_eastern}\n")
+        except Exception as e:
+            print(f"Error logging activity: {e}")
         return func(*args, **kwargs)
 
     return wrapper
