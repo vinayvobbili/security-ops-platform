020d55893be4b741eba2759e1a76c751de51e018
Vinay <vinayvobbilichetty11@gmail.com>
2024-12-17 12:19:38 -0500

    more changes

--- constants.py
+++ constants.py
@@ -1,10 +1,17 @@
-
 TICKET_TYPE_MAPPING = {
     "Prisma Cloud Compute Runtime Alert": "Prisma",
     "Third Party Compromise": "TP",
     "Qradar Alert": "QR",
     "Ticket QA": "QA",
     "Employee Reported Incident": "ER",
-    "Crowdstrike Incident": "CS I.",
-    "Crowdstrike Detection": "CS D"
+    "CrowdStrike Falcon Incident": "CS I",
+    "CrowdStrike Falcon Detection": "CS D",
+    "Akamai Alert": "Akamai",
+    "Vectra Detection": "Vectra",
+    "Splunk Alert": "Splunk"
+}
+IMPACT_MAPPING = {
+    "Ignore": "Ig",
+    "Testing": "Test",
+    "Prevented": "Pr"
 }
