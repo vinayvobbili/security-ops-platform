1f85033eee0105708d28149e6db5ded60b633f2b
Vinay <vinayvobbilichetty11@gmail.com>
2025-01-09 11:39:05 -0500

    more changes

--- heatmap.py
+++ heatmap.py
@@ -20,7 +20,7 @@ from incident_fetcher import IncidentFetcher
 from bot_rooms import get_room_name
 
 config = get_config()
-webex_api = WebexTeamsAPI(access_token=config.webex_bot_access_token)
+webex_api = WebexTeamsAPI(access_token=config.webex_bot_access_token_moneyball)
 
 eastern = timezone('US/Eastern')  # Define the Eastern time zone
 
