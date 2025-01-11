6a92a88770fd24208f91009f991db87972353a8f
Vinay <vinayvobbilichetty11@gmail.com>
2025-01-11 17:35:38 -0500

    more changes

--- web/dashboard.py
+++ web/dashboard.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 import uvicorn
 from fastapi import FastAPI, Request
@@ -6,41 +7,55 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+# Initialize FastAPI app
 app = FastAPI()
 
-# Mount static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/charts", StaticFiles(directory="../charts"), name="charts")  # Serve the 'charts' directory as well
 
 # Configure Jinja2 templates
 templates = Jinja2Templates(directory=".")
 
+# Supported image extensions
+IMAGE_EXTENSIONS = (".jpg", ".jpeg", ".png", ".gif", ".svg")
 
-# Get the list of image files
-def get_image_files():
+
+def get_image_files() -> List[str]:
+    """Retrieves a list of image files from the static and charts directories."""
     image_files = []
-    for filename in os.listdir("static/images"):  # Updated path
-        if filename.endswith((".jpg", ".jpeg", ".png", ".gif", ".svg")):  # Check for common image extensions
-            image_files.append(filename)
+
+    for directory, prefix in (("static/images", ""), ("../charts", "charts/")):  # add prefix for chart images
+        for filename in os.listdir(directory):
+            if filename.endswith(IMAGE_EXTENSIONS):
+                image_files.append(prefix + filename)  # Prefix filenames from charts directory
     return image_files
+
+
+# Pre-defined image order for display
+IMAGE_ORDER = [
+    "Company Logo.jpg",
+    "GDnR Coin.png",
+    "IR_Metrics.jpeg",
+    "Aging Tickets.png",
+    "Inflow.png",
+    "SLA Breaches.png",
+    "MTTR-MTTC.png",
+    "Outflow.png",
+    "Heatmap.png",
+    "charts/de_stories.png",
+    "charts/re_stories.png",
+    "Feedback Email.png",
+    "Thanks.png"
+]
 
 
 @app.get("/full", response_class=HTMLResponse)
 async def read_root(request: Request):
+    """Renders the HTML template with the ordered list of image files."""
+
     image_files = get_image_files()
-    order = [
-        "Company Logo.jpg",
-        "GDnR Coin.png",
-        "IR_Metrics_.jpeg",
-        "Aging Tickets.png",
-        "Inflow.png",
-        "SLA Breaches.png",
-        "MTTR-MTTC.png",
-        "Outflow.png",
-        "Heatmap.png",
-        "Feedback Email.png",
-        "Thanks.png"
-    ]
-    image_files.sort(key=lambda x: order.index(x))
+    # Sort image files according to the predefined order
+    image_files.sort(key=lambda x: IMAGE_ORDER.index(x) if x in IMAGE_ORDER else len(IMAGE_ORDER))
 
     return templates.TemplateResponse("index.html", {"request": request, "image_files": image_files})
 
