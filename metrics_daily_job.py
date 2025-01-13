50d526551ee8a71074e0aaec659a8936fa849045
Vinay <vinayvobbilichetty11@gmail.com>
2025-01-13 15:27:06 -0500

    more changes

--- metrics_daily_job.py
+++ metrics_daily_job.py
@@ -4,6 +4,8 @@ import pytz
 import schedule
 
 import aging_tickets
+import de_stories
+import re_stories
 
 
 def main():
@@ -12,7 +14,11 @@ def main():
 
     # schedule
     print("Starting the scheduler...")
-    schedule.every().day.at("08:00", pytz.timezone('US/Eastern')).do(aging_tickets.send_report)
+    schedule.every().day.at("00:01", pytz.timezone('US/Eastern')).do(lambda: (
+        aging_tickets.make_chart(),
+        de_stories.make_chart(),
+        re_stories.make_chart()
+    ))
 
     while True:
         schedule.run_pending()
