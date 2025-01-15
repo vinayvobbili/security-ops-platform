8b3b0d575560c19df2d1488e8e05e59f23c9f7cd
Vinay <vinayvobbilichetty11@gmail.com>
2025-01-15 14:50:38 -0500

    more changes

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from webex_bot.models.command import Command
 
+import days_since_incident
+import mttr_mttc
+import re_stories
 from helper_methods import log_activity
 
 
@@ -10,3 +13,8 @@ class Test(Command):
     @log_activity
     def execute(self, message, attachment_actions, activity):
         return "Test passed!"
+
+
+# mttr_mttc.make_chart()
+# re_stories.make_chart()
+days_since_incident.make_chart()
