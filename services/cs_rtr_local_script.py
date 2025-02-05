8e44cf5598716948a7f56edba6588bb5865b4ea4
Vinay <vinayvobbilichetty11@gmail.com>
2025-02-04 20:56:38 -0500

    more changes

--- services/cs_rtr_local_script.py
+++ services/cs_rtr_local_script.py
@@ -56,7 +56,7 @@ def execute_script_on_host(hostname, script_name):
         print(f"Script '{script_name}' executed successfully on host with ID '{host_id}'.")
 
         # Optionally, retrieve the results
-        result_response = rtr_api.get_command_result(session_id=session_id, sequence_id=execute_response['body']['resources'][0]['sequence_id'])
+        result_response = rtr_api.check_command_status(session_id=session_id, sequence_id=execute_response['body']['resources'][0]['sequence_id'])
         if result_response['status_code'] == 200:
             print("Command Result:", result_response['body']['resources'][0]['stdout'])
         else:
@@ -68,14 +68,9 @@ def execute_script_on_host(hostname, script_name):
 
 
 if __name__ == "__main__":
-    import sys
 
-    if len(sys.argv) != 3:
-        print("Usage: python execute_rtr_script.py <hostname> <script_name>")
-        sys.exit(1)
-
-    hostname = sys.argv[1]
-    script_name = sys.argv[2]
+    hostname = 'C02G7C7LMD6R'
+    script_name = 'test_script_mac.sh'
 
     try:
         execute_script_on_host(hostname, script_name)
