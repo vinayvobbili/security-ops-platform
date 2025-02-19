2a10e8fdd11b6ebbc539521318940341aa54a338
Vinay Vobbilichetty <vinayvobbilichetty11@gmail.com>
2025-02-18 23:03:33 -0500

    more changes

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -4,7 +4,15 @@ import pytz
 import schedule
 
 import aging_tickets
+import days_since_incident
+import de_stories
+import heatmap
+import mttr_mttc
+import outflow
+import re_stories
 import secops_shift_staffing
+import sla_breaches
+import verify_host_online_status
 
 
 def main():
@@ -17,6 +25,19 @@ def main():
         aging_tickets.send_report(),
         # abandoned_tickets.send_report(),
     ))
+
+    schedule.every().day.at("00:01", pytz.timezone('US/Eastern')).do(lambda: (
+        aging_tickets.make_chart(),
+        mttr_mttc.make_chart(),
+        sla_breaches.make_chart(),
+        de_stories.make_chart(),
+        re_stories.make_chart(),
+        days_since_incident.make_chart(),
+        outflow.make_chart(),
+        heatmap.create_choropleth_map()
+    ))
+
+    schedule.every(5).minutes.do(verify_host_online_status.start)
 
     schedule.every().day.at("03:30", pytz.timezone('US/Eastern')).do(lambda: secops_shift_staffing.announce_shift_staffing('morning'))
     schedule.every().day.at("11:30", pytz.timezone('US/Eastern')).do(lambda: secops_shift_staffing.announce_shift_staffing('afternoon'))
