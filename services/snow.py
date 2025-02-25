37086a834046c74d2d2ac53860cc9fbb507f5bad
Vinay <vinayvobbilichetty11@gmail.com>
2025-02-25 15:14:08 -0500

    more changes

--- services/snow.py
+++ services/snow.py
@@ -2,6 +2,10 @@ import base64
 
 import requests
 
+from config import get_config
+
+config = get_config()
+
 
 class ServiceNowClient:
     def __init__(self, base_url, user_name, password, client_id):
@@ -10,24 +14,22 @@ class ServiceNowClient:
         self.password = password
         self.client_id = client_id
 
-    def get_token_request(self):
+    def get_token(self):
         url = self.base_url + '/authorization/token'
         credentials = self.user_name + ':' + self.password
         cred_bytes = credentials.encode("utf-8")
         encoded_u = base64.b64encode(cred_bytes)
         header = {'Authorization': 'Basic %s' % encoded_u, 'Content-Type': 'application/json', 'X-IBM-Client-Id': self.client_id}
-        token_response = requests.get(url, headers=header, auth=(self.user_name, self.password), verify=False)
-        if not token_response:
-            err_msg = 'Authorization Error: User has no authorization to create a token.' \
-                      ' Please make sure you entered the credentials correctly.'
-            raise Exception(err_msg)
-        return token_response.json()['access_token']
+        response = requests.get(url, headers=header, auth=(self.user_name, self.password), verify=False)
+        print(response.text)
+        return response.json()['access_token']
 
 
-base_url = 'https://portal.internal.amer.apic.company.com/acme/production'
-client_id = ''
-user_name = ''
-password = ''
-client = ServiceNowClient(base_url, user_name, password, client_id)
+snow_base_url = config.snow_base_url
+client_id = config.snow_client_key
+user_name = config.snow_functional_account_id
+password = config.snow_functional_account_password
 
-print(client.get_token_request())
+client = ServiceNowClient(snow_base_url, user_name, password, client_id)
+token = client.get_token()
+print(token)
