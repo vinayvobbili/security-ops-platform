02abefe3edb9aa879237106cca7fb414fda35b2e
Vinay <vinayvobbilichetty11@gmail.com>
2025-02-27 10:45:57 -0500

    more changes

--- services/snow.py
+++ services/snow.py
@@ -1,19 +1,10 @@
 import base64
-import json
 
 import requests
 
 from config import get_config
 
-environment = 'dev'
 config = get_config()
-snow_creds = json.loads(config.snow_creds)[environment]
-CLIENT_KEY = snow_creds['CLIENT_KEY']
-CLIENT_SECRET = snow_creds['CLIENT_SECRET']
-USERNAME = snow_creds['FUNCTIONAL_ACCOUNT_ID']
-PASSWORD = snow_creds['FUNCTIONAL_ACCOUNT_PASSWORD']
-BASE_URL = snow_creds['BASE_URL']
-print(CLIENT_KEY, CLIENT_SECRET, USERNAME, PASSWORD, BASE_URL)
 
 
 class ServiceNowClient:
@@ -34,6 +25,11 @@ class ServiceNowClient:
         return response.json()['access_token']
 
 
-client = ServiceNowClient(BASE_URL, USERNAME, PASSWORD, CLIENT_KEY)
+snow_base_url = config.snow_base_url
+client_id = config.snow_client_key
+user_name = config.snow_functional_account_id
+password = config.snow_functional_account_password
+
+client = ServiceNowClient(snow_base_url, user_name, password, client_id)
 token = client.get_token()
 print(token)
