4e60b3cc953431fad36554e0b8426c81f33fa919
Vinay Vobbilichetty <vinayvobbilichetty11@gmail.com>
2025-02-28 07:31:26 -0500

    more changes

--- inflow.py
+++ inflow.py
@@ -1,49 +1,90 @@
+import json
+import re
 from datetime import datetime, timedelta
 
+import matplotlib.pyplot as plt
+import pandas as pd
 import pytz
-from webex_bot.models.command import Command
-from webexteamssdk import WebexTeamsAPI
+from matplotlib import transforms
 
-import chart
 from config import get_config
-from helper_methods import log_activity
 from incident_fetcher import IncidentFetcher
 
 eastern = pytz.timezone('US/Eastern')  # Define the Eastern time zone
-QUERY_TEMPLATE = '-category:job type:{ticket_type_prefix} -owner:"" created:>={start} created:<{end}'
 
 config = get_config()
-webex_api = WebexTeamsAPI(access_token=config.webex_bot_access_token_moneyball)
+
+with open('data/detection_source_codes_by_name.json', 'r') as f:
+    detection_source_codes_by_name = json.load(f)
 
 
-def plot_inflow() -> str:
-    # Calculate fresh values EACH TIME the command is run
-    et = pytz.timezone("US/Eastern")
+def create_bar_chart(df, x_label, y_label, title):
+    """Creates a bar chart from a pandas DataFrame."""
+    fig, ax = plt.subplots(figsize=(10, 6))
+    bars = ax.bar(df.index, df['count'], color='#153289', width=0.5)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(title, fontweight='bold', fontsize=12, )
 
-    yesterday_start = datetime.now(et).replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)
-    yesterday_end = yesterday_start + timedelta(days=1)
-    yesterday_start_utc = yesterday_start.astimezone(pytz.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
-    yesterday_end_utc = yesterday_end.astimezone(pytz.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+    # Add count labels on top of each bar
+    for bar in bars:
+        yval = bar.get_height()
+        ax.text(bar.get_x() + bar.get_width() / 2, yval, yval, va='bottom', ha='center',
+                fontdict={'fontsize': 10, 'fontweight': 'bold'})
 
-    # Use an f-string or format to create the query dynamically
-    query = QUERY_TEMPLATE.format(ticket_type_prefix=config.ticket_type_prefix, start=yesterday_start_utc, end=yesterday_end_utc)
-    tickets = IncidentFetcher().get_tickets(query=query)
-    filepath = chart.make_pie(tickets, 'Inflow Yesterday')  # Store the full path
-
-    return filepath
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    return fig
 
 
-class Inflow(Command):
+def add_timestamp(fig, now_eastern):
+    """Adds a timestamp to the chart."""
+    trans = transforms.blended_transform_factory(fig.transFigure, fig.transFigure)
+    plt.text(0.08, 0.03, now_eastern, ha='left', va='bottom', fontsize=10, transform=trans)
 
-    def __init__(self):
-        super().__init__(command_keyword="inflow", help_message="Inflow")
 
-    @log_activity
-    def execute(self, message, attachment_actions, activity):
-        inflow_chart_filepath = plot_inflow()
+def plot_inflow():
+    """Plots the ticket inflow by source using pandas."""
+    query = f'-category:job type:{config.ticket_type_prefix} -owner:""'
+    # Unable to build a period that fetches only yesterday's tickets. Work around: Fetch both today's and yesterday's tickets and filter out today's
+    period = {"byFrom": "days", "fromValue": 1, "byTo": "days", "toValue": 0}
 
-        webex_api.messages.create(
-            roomId=attachment_actions.json_data["roomId"],
-            text=f"{activity['actor']['displayName']}, here's the Inflow chart!",
-            files=[inflow_chart_filepath]  # Path to the file
-        )
+    tickets = IncidentFetcher().get_tickets(query=query, period=period)
+
+    yesterday = (datetime.now(eastern) - timedelta(days=1)).date()
+    tickets = [ticket for ticket in tickets if datetime.strptime(ticket.get('created'), '%Y-%m-%dT%H:%M:%S.%fZ').date() == yesterday]
+
+    # Create a DataFrame from the tickets
+    if not tickets:
+        print('No tickets found matching the current query')
+        return
+
+    df = pd.DataFrame(tickets)
+
+    # Extract the 'detectionsource' from the 'CustomFields' dictionary
+    df['source'] = df['CustomFields'].apply(lambda x: x.get('detectionsource'))
+
+    for pattern, replacement in detection_source_codes_by_name.items():
+        df['source'] = df['source'].str.replace(pattern, replacement, regex=True, flags=re.IGNORECASE)
+
+    # Count the occurrences of each source
+    source_counts = df['source'].value_counts().rename_axis('source').reset_index(name='count')
+    source_counts = source_counts.set_index('source')
+
+    # Create the bar chart
+    fig = create_bar_chart(source_counts, "Detection Source", "Number of Alerts", "Inflow Yesterday")
+
+    # Add a thin black border around the figure
+    fig.patch.set_edgecolor('black')
+    fig.patch.set_linewidth(10)
+
+    # Add the current time to the chart
+    now_eastern = datetime.now(eastern).strftime('%m/%d/%Y %I:%M %p %Z')
+    add_timestamp(fig, now_eastern)
+
+    fig.savefig('web/static/charts/Inflow.png')
+    plt.close(fig)
+
+
+if __name__ == '__main__':
+    plot_inflow()
