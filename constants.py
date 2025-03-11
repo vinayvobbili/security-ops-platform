c4ec26494fb98a25c246d29fe3529c1b30dabbd6
Vinay <vinayvobbilichetty11@gmail.com>
2025-03-11 18:27:20 -0400

    more changes

--- constants.py
+++ constants.py
@@ -1,20 +1,14 @@
 TICKET_TYPE_MAPPING = {
     "Prisma Cloud Compute Runtime Alert": "Pr. Comp",
     "UEBA Prisma Cloud": "Pr. UEBA",
-    "Third Party Compromise": "TP",
-    "Qradar Alert": "QR",
+    "Third Party Compromise": "Third Party",
+    "Qradar Alert": "QRadar",
     "Ticket QA": "QA",
     "Employee Reported Incident": "ER",
     "CrowdStrike Falcon Incident": "CS I",
     "CrowdStrike Falcon Detection": "CS D",
     "Akamai Alert": "Akamai",
     "Vectra Detection": "Vectra",
-    "Splunk Alert": "Splunk"
-}
-IMPACT_MAPPING = {
-    "Ignore": "Ig",
-    "Testing": "Test",
-    "Prevented": "Prev",
-    "False Positive": "FP",
-    "Confirmed": "Con"
+    "Splunk Alert": "Splunk",
+    "Lost or Stolen Computer": "Lost/Stolen"
 }
