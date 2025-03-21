5753a02827df9f4bf74ff27ea3208fc91b68fb14
Vinay <vinayvobbilichetty11@gmail.com>
2025-03-21 16:46:13 -0400

    more changes

--- src/helper_methods.py
+++ src/helper_methods.py
@@ -4,8 +4,8 @@ from functools import wraps
 
 from pytz import timezone
 
-from services.bot_rooms import get_room_name
 from config import get_config
+from services.bot_rooms import get_room_name
 
 eastern = timezone('US/Eastern')
 
@@ -76,3 +76,33 @@ def log_jarvais_activity(bot_access_token):
     return decorator
 
 
+def log_soar_activity(bot_access_token):
+    """
+    Decorator that logs activity, using the provided bot access token.
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            attachment_actions = args[2]
+            activity = args[3]
+
+            now_eastern = datetime.now(eastern).strftime('%m/%d/%Y %I:%M:%S %p %Z')
+            try:
+                actor = activity["actor"]["displayName"]
+                if actor is not config.my_name:
+                    with open("../data/transient/logs/soar_activity_log.csv", "a", newline="") as f:
+                        writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL)  # Use csv.writer for proper quoting
+                        writer.writerow([
+                            f'"{activity["actor"]["displayName"]}"',  # Quote the name field
+                            attachment_actions.json_data.get('inputs', {}).get('command_keyword') or attachment_actions.json_data['text'],
+                            get_room_name(attachment_actions.json_data['roomId'], bot_access_token),
+                            now_eastern
+                        ])
+            except Exception as e:
+                print(f"Error logging activity: {e}")
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
