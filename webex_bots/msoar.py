88972a10ba8dcc38fe841957f7f3c8d5e6793ac3
Vinay Vobbilichetty <vinayvobbilichetty11@gmail.com>
2025-03-22 07:44:07 -0400

    more changes

--- webex_bots/msoar.py
+++ webex_bots/msoar.py
@@ -1,3 +1,5 @@
+import json
+
 from webex_bot.models.command import Command
 from webex_bot.webex_bot import WebexBot
 from webexteamssdk import WebexTeamsAPI
@@ -9,6 +11,9 @@ config = get_config()
 bot_token = config.webex_bot_access_token_soar
 webex_api = WebexTeamsAPI(access_token=bot_token)
 
+NOTES_FILE = "../data/transient/secOps/management_notes.txt"
+THREAT_CON_FILE = "../data/transient/secOps/threatcon.json"
+
 
 # Command to save notes
 class SaveNotes(Command):
@@ -16,7 +21,7 @@ class SaveNotes(Command):
         super().__init__(command_keyword="save_notes")
 
     def execute(self, message, attachment_actions, activity):
-        with open("../data/transient/notes/management_notes.txt", "w") as file:
+        with open(NOTES_FILE, "w") as file:
             file.write(attachment_actions.inputs['management_notes'])
 
         card = {
@@ -49,7 +54,7 @@ class ManagementNotes(Command):
 
     @log_soar_activity(bot_access_token=bot_token)
     def execute(self, message, attachment_actions, activity):
-        with open("../data/transient/notes/management_notes.txt", "r") as file:
+        with open(NOTES_FILE, "r") as file:
             content = file.read()
 
         card = {
@@ -77,12 +82,118 @@ class ManagementNotes(Command):
                     "horizontalAlignment": "Right"
                 }
             ],
-            "version": "1.0"
+            "version": "1.3"
         }
 
         webex_api.messages.create(
             toPersonEmail=activity['actor']['id'],
             text='Management Notes',
+            attachments=[{"contentType": "application/vnd.microsoft.card.adaptive", "content": card}]
+        )
+
+
+# Command to update threatcon level
+class SaveThreatcon(Command):
+    def __init__(self):
+        super().__init__(command_keyword="save_threatcon")
+
+    def execute(self, message, attachment_actions, activity):
+        level = attachment_actions.inputs['threatcon_level']
+        reason = attachment_actions.inputs['reason']
+
+        threatcon_details = {
+            "level": level,
+            "reason": reason
+        }
+
+        with open(THREAT_CON_FILE, "w") as file:
+            json.dump(threatcon_details, file, indent=4)
+
+        card = {
+            "type": "AdaptiveCard",
+            "body": [
+                {
+                    "type": "TextBlock",
+                    "text": "ThreatCon Level Updated Successfully",
+                    "weight": "Bolder"
+                },
+                {
+                    "type": "TextBlock",
+                    "text": f"ThreatCon Level: {level}",
+                },
+                {
+                    "type": "TextBlock",
+                    "text": f"Reason: {reason}",
+                }
+            ],
+            "version": "1.0"
+        }
+
+        webex_api.messages.create(
+            toPersonEmail=activity['actor']['id'],
+            text='ThreatCon Level Updated Successfully',
+            attachments=[{"contentType": "application/vnd.microsoft.card.adaptive", "content": card}]
+        )
+
+
+class ThreatconLevel(Command):
+    def __init__(self):
+        super().__init__(command_keyword="threatcon", help_message="ThreatCon Level")
+
+    @log_soar_activity(bot_access_token=bot_token)
+    def execute(self, message, attachment_actions, activity):
+        with open(THREAT_CON_FILE, "r") as file:
+            threatcon_details = file.read()
+
+        threatcon_details = json.loads(threatcon_details)
+        level = threatcon_details.get('level', 'Umnknown Level')
+        reason = threatcon_details.get('reason', 'Unknown Reason')
+
+        card = {
+            "type": "AdaptiveCard",
+            "body": [
+                {
+                    "type": "TextBlock",
+                    "text": "ThreatCon Level",
+                    "weight": "Bolder"
+                },
+                {
+                    "type": "Input.ChoiceSet",
+                    "id": "threatcon_level",
+                    "value": level,
+                    "label": "Level",
+                    "choices": [
+                        {"title": "Green", "value": "green"},
+                        {"title": "Yellow", "value": "yellow"},
+                        {"title": "Orange", "value": "orange"},
+                        {"title": "Red", "value": "red"}
+                    ],
+                    "style": "compact"
+                },
+                {
+                    "type": "Input.Text",
+                    "id": "reason",
+                    "label": "Reason",
+                    "isMultiline": True,
+                    "value": reason,
+                    "placeholder": "Enter reason here"
+                }
+            ],
+            "actions": [
+                {
+                    "type": "Action.Submit",
+                    "title": "Update",
+                    "style": "positive",
+                    "horizontalAlignment": "Right",
+                    "data": {"callback_keyword": "save_threatcon"}
+                }
+            ],
+            "version": "1.3"
+        }
+
+        webex_api.messages.create(
+            toPersonEmail=activity['actor']['id'],
+            text='Threatcon Level',
             attachments=[{"contentType": "application/vnd.microsoft.card.adaptive", "content": card}]
         )
 
@@ -95,7 +206,9 @@ def run_bot():
             bot_name="Hello, Manager!"
         )
         bot.add_command(ManagementNotes())
+        bot.add_command(ThreatconLevel())
         bot.add_command(SaveNotes())
+        bot.add_command(SaveThreatcon())
         bot.run()
     except Exception as e:
         print(f"Bot failed to start: {e}")
