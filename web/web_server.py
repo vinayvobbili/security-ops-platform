17db7185739d36ad38c777a19a00ddbe88d43343
Vinay <vinayvobbilichetty11@gmail.com>
2025-03-25 15:55:04 -0400

    more changes

--- web/web_server.py
+++ web/web_server.py
@@ -17,7 +17,7 @@ CONFIG = get_config()
 
 # Supported image extensions
 IMAGE_EXTENSIONS = (".jpg", ".jpeg", ".png", ".gif", ".svg")
-blocked_ips = ["10.49.70.105", "10.50.70.91", "192.168.1.100"]
+blocked_ips = ["10.49.70.105", "10.50.70.91"]
 
 
 @app.before_request
