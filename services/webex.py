e53a317aaad57fc3ada42056f49ab3888296120d
Vinay <vinayvobbilichetty11@gmail.com>
2025-04-03 18:21:46 -0400

    more changes

--- services/webex.py
+++ services/webex.py
@@ -11,6 +11,6 @@ webex_api = WebexTeamsAPI(access_token=config.webex_bot_access_token_moneyball)
 """Sends a chart image to a Webex room."""
 webex_api.messages.create(
     roomId=config.webex_room_id_metrics,
-    text=f"Vectra Efficacy chart!",
-    files=['/Users/user/PycharmProjects/IR/web/static/charts/Vectra Efficacy.png']
+    text=f"CrowdStrike Efficacy chart!",
+    files=['/Users/user/PycharmProjects/IR/web/static/charts/CrowdStrike Efficacy.png']
 )
