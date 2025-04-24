b8fedf8f7044109766a174182cb7ecaf9ada37e1
Vinay Vobbilichetty <vinayvobbilichetty11@gmail.com>
2025-04-23 23:54:28 -0400

    more changes

--- services/webex.py
+++ services/webex.py
@@ -6,13 +6,13 @@ from config import get_config
 config = get_config()
 
 # Initialize Webex API client
-webex_api = WebexTeamsAPI(access_token=config.webex_bot_access_token_toodles)
+webex_api = WebexTeamsAPI(access_token=config.webex_bot_access_token_moneyball)
 
 """Sends a file to a Webex room."""
 webex_api.messages.create(
-    roomId=config.webex_room_id_threatcon_collab,
-    text=f"The new chart shown in the meeting today!",
+    roomId=config.webex_room_id_metrics,
+    text=f"A new chart!",
     files=[
-        '/Users/user/PycharmProjects/IR/web/static/charts/04-21-2025/CS Detection Efficacy-Month.png'
+        '/Users/user/PycharmProjects/IR/web/static/charts/04-23-2025/Inflow Past 12 Months.png'
     ]
 )
