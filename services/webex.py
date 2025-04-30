e761530e1582f0c8f892ddf9fb06df64a5579476
Vinay Vobbilichetty <vinayvobbilichetty11@gmail.com>
2025-04-30 14:56:23 -0400

    more changes

--- services/webex.py
+++ services/webex.py
@@ -11,8 +11,8 @@ webex_api = WebexTeamsAPI(access_token=config.webex_bot_access_token_toodles)
 """Sends a file to a Webex room."""
 webex_api.messages.create(
     roomId=config.webex_room_id_threatcon_collab,
-    text=f"It's good to see the volume decreasing after two months of heavy inflow❣️👌🏾",
+    text=f"Chart for today's FP review meeting",
     files=[
-        '/Users/user/PycharmProjects/IR/web/static/charts/04-29-2025/Inflow Past 12 Months - Impact Only.png'
+        '/Users/user/PycharmProjects/IR/web/static/charts/04-30-2025/CS Detection Efficacy-Month.png'
     ]
 )
