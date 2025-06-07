53280563a33fa124c1f7ad16a06037907f69b7ca
vvobbilichetty <vinayvobbilichetty11@gmail.com>
2025-06-06 22:23:36 -0400

    more changes

--- services/webex.py
+++ services/webex.py
@@ -10,7 +10,7 @@ webex_api = WebexTeamsAPI(access_token=config.webex_bot_access_token_jarvais)
 
 """Sends a file to a Webex room."""
 webex_api.messages.create(
-    roomId=config.webex_room_id_epp_tagging,
+    roomId=config.webex_room_id_vinay_test_space,
     text=f"CS servers with Invalid Ring tags. Please review. Count=364",
     files=[
         '/Users/user/PycharmProjects/IR/data/transient/epp_device_tagging/06-06-2025/cs_servers_with_invalid_ring_tags_only.xlsx'
