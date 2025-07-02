b55fa84c757b05fe2483dd81626813b8528e96d0
vvobbilichetty <vinayvobbilichetty11@gmail.com>
2025-07-02 19:36:57 -0400

    more changes

---
+++ src/components/incident_declaration_sla_risk.py
@@ -0,0 +1,34 @@
+from webexpythonsdk import WebexAPI
+
+from config import get_config
+from services.xsoar import TicketHandler
+
+CONFIG = get_config()
+webex_api = WebexAPI(access_token=CONFIG.webex_bot_access_token_soar)
+
+
+def start(room_id):
+    ticket_handler = TicketHandler()
+    query = '-status:closed -category:job type:METCIRT metcirtincidentnotificationsla.runStatus:running metcirtincidentnotificationsla.slaStatus:risk'
+    # query = '-category:job type:METCIRT timetorespond.runStatus:running'
+    tickets = ticket_handler.get_tickets(query)
+    if not tickets:
+        return
+    message = []
+    for ticket in tickets:
+        incident_url = CONFIG.xsoar_prod_ui_base_url + '/Custom/caseinfoid/' + ticket.get('id')
+        ticket_id = ticket.get('id')
+        ticket_name = ticket.get('name') or ticket.get('title') or 'No Title'
+        message.append(f"- [**{ticket_id}**]({incident_url}) - {ticket_name}")
+    markdown_header = "## ⚠️ Tickets at risk of Incident Declaration SLA breach 🚨"
+    markdown_message = "\n".join(message)
+    webex_api.messages.create(
+        roomId=room_id,
+        text="Tickets at the risk of breaching Incident Declaration SLA",
+        markdown=f"{markdown_header}\n\n{markdown_message}"
+    )
+
+
+if __name__ == "__main__":
+    room_id = CONFIG.webex_room_id_vinay_test_space
+    start(room_id)
