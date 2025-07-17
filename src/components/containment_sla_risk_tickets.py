3df46731c3971648586746f5ee0c4e092b9fe65c
vvobbilichetty <vinayvobbilichetty11@gmail.com>
2025-07-16 21:31:55 -0400

    more changes

--- src/components/containment_sla_risk_tickets.py
+++ src/components/containment_sla_risk_tickets.py
@@ -8,25 +8,28 @@ webex_api = WebexAPI(access_token=CONFIG.webex_bot_access_token_soar)
 
 
 def start(room_id):
-    ticket_handler = TicketHandler()
-    query = '-status:closed -category:job type:METCIRT timetocontain.runStatus:running timetocontain.slaStatus:2 -hostname:""'
-    # query = '-category:job type:METCIRT timetocontain.runStatus:running'
-    tickets = ticket_handler.get_tickets(query)
-    if not tickets:
-        return
-    message = []
-    for ticket in tickets:
-        incident_url = CONFIG.xsoar_prod_ui_base_url + '/Custom/caseinfoid/' + ticket.get('id')
-        ticket_id = ticket.get('id')
-        ticket_name = ticket.get('name') or ticket.get('title') or 'No Title'
-        message.append(f"- [**{ticket_id}**]({incident_url}) - {ticket_name}")
-    markdown_header = "## ⚠️ Tickets at risk of Containment SLA breach 🚨"
-    markdown_message = "\n".join(message)
-    webex_api.messages.create(
-        roomId=room_id,
-        text="Tickets at the risk of breaching response SLA",
-        markdown=f"{markdown_header}\n\n{markdown_message}"
-    )
+    try:
+        ticket_handler = TicketHandler()
+        query = '-status:closed -category:job type:METCIRT timetocontain.runStatus:running timetocontain.slaStatus:2 -hostname:""'
+        # query = '-category:job type:METCIRT timetocontain.runStatus:running'
+        tickets = ticket_handler.get_tickets(query)
+        if not tickets:
+            return
+        message = []
+        for ticket in tickets:
+            incident_url = CONFIG.xsoar_prod_ui_base_url + '/Custom/caseinfoid/' + ticket.get('id')
+            ticket_id = ticket.get('id')
+            ticket_name = ticket.get('name') or ticket.get('title') or 'No Title'
+            message.append(f"- [**{ticket_id}**]({incident_url}) - {ticket_name}")
+        markdown_header = "## ⚠️ Tickets at risk of Containment SLA breach 🚨"
+        markdown_message = "\n".join(message)
+        webex_api.messages.create(
+            roomId=room_id,
+            text="Tickets at the risk of breaching response SLA",
+            markdown=f"{markdown_header}\n\n{markdown_message}"
+        )
+    except Exception as e:
+        print(f"Error processing tickets: {e}")
 
 
 if __name__ == "__main__":
