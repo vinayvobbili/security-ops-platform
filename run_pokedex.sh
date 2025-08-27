dd962fce78bdb48100486b48f6b605810bf363a3
vvobbilichetty <vinayvobbilichetty11@gmail.com>
2025-08-27 07:22:01 -0400

    more changes

---
+++ run_pokedex.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Pokedex SOC Bot Runner Script
+
+# Save current directory
+ORIGINAL_DIR=$(pwd)
+
+# Change to project directory
+cd /Users/user/PycharmProjects/IR || exit 1
+
+# Activate virtual environment and run bot
+source .venv/bin/activate
+export PYTHONPATH="/Users/user/PycharmProjects/IR:$PYTHONPATH"
+python webex_bots/pokedex.py
+
+# Return to original directory
+cd "$ORIGINAL_DIR" || exit 1
