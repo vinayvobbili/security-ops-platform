1c4811b9b7d5ce72a3b1bc19a705f313f1e575df
vvobbilichetty <vinayvobbilichetty11@gmail.com>
2025-08-28 12:22:18 -0400

    more changes

--- src/pokedex/run_pokedex.sh
+++ src/pokedex/run_pokedex.sh
@@ -29,7 +29,7 @@ fi
 echo "🤖 Starting the security assistant bot bot..."
 source .venv/bin/activate
 export PYTHONPATH="/Users/user/PycharmProjects/IR:$PYTHONPATH"
-python webex_bots/pokedex.py
+python src/pokedex/pokedex.py
 
 # Return to original directory
 cd "$ORIGINAL_DIR" || exit 1
