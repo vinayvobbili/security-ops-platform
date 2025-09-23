1e1b5707a127175fad45570dbc8703b0c79d44fc
vvobbilichetty <vinayvobbilichetty11@gmail.com>
2025-09-23 00:06:59 -0400

    more changes

--- src/all_jobs.py
+++ src/all_jobs.py
@@ -6,12 +6,11 @@ import schedule
 
 import secops
 from my_config import get_config
-from services import phish_fort
+from services.xsoar import TicketHandler  # Added import for caching past 90 days tickets
 from src import helper_methods, verify_host_online_status
 from src.charts import mttr_mttc, outflow, lifespan, heatmap, sla_breaches, aging_tickets, inflow, qradar_rule_efficacy, de_stories, days_since_incident, re_stories, threatcon_level, vectra_volume, \
     crowdstrike_volume, threat_tippers, crowdstrike_efficacy
-from src.components import oncall, approved_security_testing, thithi, orphaned_tickets, qa_tickets, response_sla_risk_tickets, containment_sla_risk_tickets, incident_declaration_sla_risk
-from src.utils.data_cache import fetch_daily_cache
+from src.components import oncall, approved_security_testing, thithi, qa_tickets, response_sla_risk_tickets, containment_sla_risk_tickets, incident_declaration_sla_risk
 from src.utils.fs_utils import make_dir_for_todays_charts
 
 logging.basicConfig(level=logging.ERROR)
@@ -61,7 +60,7 @@ def main():
 
     schedule.every().day.at("00:01", eastern).do(lambda: (
         make_dir_for_todays_charts(helper_methods.CHARTS_DIR_PATH),
-        fetch_daily_cache(),  # Fetch and cache 3 months of data first
+        TicketHandler().cache_past_90_days_tickets(),  # Cache past 90 days tickets from XSOAR
         aging_tickets.make_chart(),
         crowdstrike_efficacy.make_chart(),
         crowdstrike_volume.make_chart(),
