29b5c9cd6cfad1747d149e2a1ed0293d7c9c6dcf
vvobbilichetty <vinayvobbilichetty11@gmail.com>
2025-10-01 14:57:15 -0400

    more changes

---
+++ web/__init__.py
@@ -0,0 +1,9 @@
+"""Web application package for IR project.
+
+Provides Flask app and related routes in web_server.py.
+This file ensures the 'web' directory is treated as a standard package so you can run:
+    python -m web.web_server --port 8000
+"""
+
+__all__ = ["web_server"]
+
