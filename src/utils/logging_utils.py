3b39141c8092cdce23e0da062fecff7bfa739167
vvobbilichetty <vinayvobbilichetty11@gmail.com>
2025-10-07 06:55:39 -0400

    more changes

--- src/utils/logging_utils.py
+++ src/utils/logging_utils.py
@@ -8,7 +8,7 @@ from flask import request
 from pytz import timezone
 
 from my_config import get_config
-from services.bot_rooms import get_room_name
+from services.webex import get_room_name
 
 eastern = timezone('US/Eastern')
 config = get_config()
